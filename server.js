c0c6cf44cd169b57159123b5374f73acd87bb3b5
LoveneetGoyal <31528900+LoveneetGoyal@users.noreply.github.com>
2017-09-02 22:23:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ui/article-one.html', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/ui/article-two.html', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/ui/article-three.html', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send("Article three requested and will be served here");
 });
 
